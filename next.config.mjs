7dd4ce09dd2dc42100f06a7594a913ccce18925f
Rao Ali Murad <136970547+Raoalimurad@users.noreply.github.com>
2024-02-22 12:05:06 +0500

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    env: {
+        CONTENTFUL_SPACE_ID: "x0j7qn0succf",
+        CONTENTFUL_ACCESS_KEY: "BQziqYnkaXTnNydtqnNubySbevUKvbYcbZNNeMp9SII"
+      },
+};
+
 
 export default nextConfig;
